b1f91e5cbf030ad519bf4b0a0e3efad14d100628
Maksim-de <arhipovm926@gmail.com>
2025-07-13 18:43:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 # Чтение токена из переменной окружения
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 TOKEN_DEPS= os.getenv("TOKEN_DEPS")
-host = os.getenv("host")
-port = os.getenv("port")
-database = os.getenv("database")
-user = os.getenv("user")
-password = os.getenv("password")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
+database = os.getenv("DATABASE")
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
 
 if not BOT_TOKEN:
     raise ValueError("Переменная окружения BOT_TOKEN не установлена!")
