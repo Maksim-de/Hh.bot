fc3339dc96f4b99ba060a96264716f51758d3915
Maksim-de <arhipovm926@gmail.com>
2025-07-15 22:32:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,27 @@
 import os
 from dotenv import load_dotenv
+import logging
 
 # Загрузка переменных из .env файла
 load_dotenv()
 
 # Чтение токена из переменной окружения
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-TOKEN_DEPS= os.getenv("TOKEN_DEPS")
+TOKEN_DEPS_ONE = os.getenv("TOKEN_DEPS_ONE")
+TOKEN_DEPS_TWO = os.getenv("TOKEN_DEPS_TWO")
+TOKEN_DEPS_THREE = os.getenv("TOKEN_DEPS_THREE")
+TOKEN_DEPS_FOUR = os.getenv("TOKEN_DEPS_FOUR")
+
 host = os.getenv("HOST")
 port = os.getenv("PORT")
 database = os.getenv("DATABASE")
 user = os.getenv("USER")
 password = os.getenv("PASSWORD")
+
+
+# Настройка логов
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 if not BOT_TOKEN:
     raise ValueError("Переменная окружения BOT_TOKEN не установлена!")
